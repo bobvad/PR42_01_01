2d3ee41faf620369104b62e765c0443a480e1c0b
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-10-03 15:33:24 +0500

    Разработанное окно.Дегтянников

--- Shops_Content_Degtinnikov/MainWindow.xaml.cs
+++ Shops_Content_Degtinnikov/MainWindow.xaml.cs
@@ -16,9 +16,18 @@ namespace Shops_Content_Degtinnikov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public View.Main Main = new View.Main();
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(Main);
+        }
+
+        private void OpenIndex(object sender, MouseButtonEventArgs e)
+        {
+            frame.Navigate(Main);
         }
     }
 }
