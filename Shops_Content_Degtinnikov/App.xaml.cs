e0465842c21123834207179a0128f7b8eece0b31
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-10-03 15:09:54 +0500

    Добавьте файлы проекта.

---
+++ Shops_Content_Degtinnikov/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Shops_Content_Degtinnikov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
